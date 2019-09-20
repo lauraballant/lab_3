0943800bc9d3b4cffc19d1a048404496882991a7
lauraballant <54919894+lauraballant@users.noreply.github.com>
2019-09-19 18:00:46 -0600

    Update lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -9,6 +9,14 @@ read regex_input filename
 
 grep $regex_input $filename
 
+#number of phone numbers
 grep -c -P '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+
+#a count of the number of emails
+grep -c -P '[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+' regex_practice.txt
+
+#lists all the phone numbers in the '303' area code
 grep -o -P '303-[0-9]{3}-[0-9]{4}' regex_practice.txt
+
+#store all the list of emails from geocities.com in email_results.txt
 grep  -P '[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+' regex_practice.txt >> email_results.txt
